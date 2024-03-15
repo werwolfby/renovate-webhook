f03a53d532be3562f3c55cea4a7d856821ea421c
Alexander Puzynia <werwolf.by@gmail.com>
2024-03-14 22:00:29 -0700

    refactor(runner): use executor in runner

--- RenovateWebhooks/RunnerOptions.cs
+++ RenovateWebhooks/RunnerOptions.cs
@@ -2,9 +2,5 @@
 
 public class RunnerOptions
 {
-    public string ExecutablePath { get; set; } = null!;
-
-    public string[] Arguments { get; set; } = Array.Empty<string>();
-
     public TimeSpan Schedule { get; set; } = TimeSpan.FromHours(1);
 }
