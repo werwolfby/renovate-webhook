95d07692dddcc3f3e289817a82771817cc6f64af
Alexander Puzynia <werwolf.by@gmail.com>
2024-03-14 21:43:20 -0700

    refactor(runner): use single channel only

--- RenovateWebhooks/Runner.cs
+++ RenovateWebhooks/Runner.cs
@@ -14,74 +14,73 @@ public class Runner(IOptions<RunnerOptions> runnerOptions, ILogger<Runner> logge
     private static readonly object Event = new();
     private readonly RunnerOptions _runnerOptions = runnerOptions.Value;
 
-    private readonly Channel<object> _triggerChannel = Channel.CreateBounded<object>(new BoundedChannelOptions(1)
+    private readonly Channel<object> _executeChannel = Channel.CreateUnbounded<object>(new UnboundedChannelOptions
     {
         SingleReader = true,
         SingleWriter = false,
         AllowSynchronousContinuations = false,
-        FullMode = BoundedChannelFullMode.DropOldest,
-    });
-    private readonly Channel<object> _cronJobChannel = Channel.CreateUnbounded<object>(new UnboundedChannelOptions
-    {
-        SingleReader = true,
-        SingleWriter = true,
-        AllowSynchronousContinuations = false,
-    });
-    private readonly Channel<object> _executeChannel = Channel.CreateUnbounded<object>(new UnboundedChannelOptions
-    {
-        SingleReader = true,
-        SingleWriter = true,
-        AllowSynchronousContinuations = false,
     });
 
-    public void Trigger() => _triggerChannel.Writer.TryWrite(Event);
+    public void Trigger() => _executeChannel.Writer.TryWrite(Event);
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _ = CronJobProducer(stoppingToken);
-        _ = ExecuteProducer(stoppingToken);
+        logger.LogInformation("Runner started");
 
-        await foreach (var _ in _executeChannel.Reader.ReadAllAsync(stoppingToken))
-        {
-            await RunExternalExecutableAsync(stoppingToken);
-        }
+        var cronJobTask = CronJobProducer(stoppingToken);
+
+        await ExecuteJob(() => RunExternalExecutableAsync(stoppingToken));
+
+        await cronJobTask;
+
+        logger.LogInformation("Runner finished");
     }
 
-    private async Task ExecuteProducer(CancellationToken cancellationToken)
+    private async Task ExecuteJob(Func<Task> run)
     {
-        var triggerReadTask = _triggerChannel.Reader.ReadAsync(cancellationToken).AsTask();
-        var cronJobReadTask = _cronJobChannel.Reader.ReadAsync(cancellationToken).AsTask();
-
-        while (!cancellationToken.IsCancellationRequested)
+        await foreach (var _ in _executeChannel.Reader.ReadAllAsync())
         {
-            var completedTask = await Task.WhenAny(triggerReadTask, cronJobReadTask);
-
-            if (completedTask == triggerReadTask)
+            try
             {
-                await _executeChannel.Writer.WriteAsync(Event, cancellationToken);
-                triggerReadTask = _triggerChannel.Reader.ReadAsync(cancellationToken).AsTask();
+                logger.LogInformation("Exec started");
+                await run();
+                logger.LogInformation("Exec completed");
             }
-            else if (completedTask == cronJobReadTask)
+            catch (OperationCanceledException)
             {
-                await _executeChannel.Writer.WriteAsync(Event, cancellationToken);
-                cronJobReadTask = _cronJobChannel.Reader.ReadAsync(cancellationToken).AsTask();
+                logger.LogInformation("Job cancelled");
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Error running job");
             }
         }
+
+        logger.LogInformation("Job execution finished");
     }
 
     private async Task CronJobProducer(CancellationToken cancellationToken)
     {
         try
         {
-            while (true)
+            while (!cancellationToken.IsCancellationRequested)
             {
-                await _cronJobChannel.Writer.WriteAsync(Event, cancellationToken);
+                await _executeChannel.Writer.WriteAsync(Event, cancellationToken);
                 await Task.Delay(_runnerOptions.Schedule, cancellationToken);
             }
         }
-        catch (OperationCanceledException)
+        catch (OperationCanceledException e)
         {
-            _cronJobChannel.Writer.Complete();
+            if (e.CancellationToken != cancellationToken)
+            {
+                throw;
+            }
+
+            logger.LogInformation("CronJobProducer cancelled");
+        }
+        finally
+        {
+            _executeChannel.Writer.Complete();
         }
     }
 
