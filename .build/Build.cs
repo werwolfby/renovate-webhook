1f7454d7fd4c2ba87920994e754a64a065c18930
Alexander Puzynia <werwolf.by@gmail.com>
2025-09-07 10:07:17 -0700

    build(nuke): add Docker target

--- .build/Build.cs
+++ .build/Build.cs
@@ -1,7 +1,10 @@
 using Nuke.Common;
 using Nuke.Common.IO;
+using Nuke.Common.Tools.Docker;
 using Nuke.Common.Utilities.Collections;
 using Nuke.Components;
+using Serilog;
+using static Nuke.Common.Tools.Docker.DockerTasks;
 
 class Build : NukeBuild, IRestore, ICompile, IHazNerdbankGitVersioning
 {
@@ -16,6 +19,28 @@ class Build : NukeBuild, IRestore, ICompile, IHazNerdbankGitVersioning
                 // Exclude .build itself
                 .WhereNot(e => e.ToString().StartsWith(BuildProjectDirectory!.ToString()))
                 .DeleteDirectories();
+        });
+
+    Target Docker => _ => _
+        .DependsOn<ICompile>()
+        .Executes(() =>
+        {
+            var version = ((IHazNerdbankGitVersioning)this).Versioning.SimpleVersion;
+            var imageName = "werwolfby/renovate-webhook";
+            var dockerTag = $"{imageName}:{version}";
+            var latestTag = $"{imageName}:latest";
+
+            DockerBuildxBuild(s => s
+                .SetPath(RootDirectory)
+                .SetFile(RootDirectory / "Dockerfile")
+                .SetTag(dockerTag, latestTag)
+                .SetPull(true)
+                .SetPlatform("linux/amd64,linux/arm64"));
+
+            ReportSummary(c => c
+                .AddPair("Docker Image", dockerTag));
+
+            Log.Logger.Information("Built Docker image {DockerTag}", dockerTag);
         });
 
 }
