cc1e07472eff34142990d3db62a71a324b34c909
Alexander Puzynia <werwolf.by@gmail.com>
2025-09-16 16:36:24 -0700

    feat(iac): add github action secrets for docker

--- .iac/github.tf
+++ .iac/github.tf
@@ -19,3 +19,15 @@ resource "github_repository" "renovate_webhook" {
   delete_branch_on_merge = true
   auto_init              = false
 }
+
+resource "github_actions_secret" "dockerhub_username" {
+  repository      = github_repository.renovate_webhook.name
+  secret_name     = "DOCKERHUB_USERNAME"
+  plaintext_value = docker_hub_repository.renovate_webhook.namespace
+}
+
+resource "github_actions_secret" "dockerhub_password" {
+  repository      = github_repository.renovate_webhook.name
+  secret_name     = "DOCKERHUB_PASSWORD"
+  plaintext_value = docker_access_token.githug_token.token
+}
