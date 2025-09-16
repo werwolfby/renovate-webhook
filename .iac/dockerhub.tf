39021e1062b976464942b4bb33fda043e216a9ec
Alexander Puzynia <werwolf.by@gmail.com>
2025-09-16 16:57:34 -0700

    build(iac): fix dockerhub pal scopes to allow push

--- .iac/dockerhub.tf
+++ .iac/dockerhub.tf
@@ -11,8 +11,6 @@ resource "docker_access_token" "githug_token" {
   token_label = "GitHub ${docker_hub_repository.renovate_webhook.namespace}/${docker_hub_repository.renovate_webhook.name}"
   
   scopes = [
-    "repo:read",
-    "repo:write", 
-    "repo:public_read"
+    "repo:admin",
   ]
 }
